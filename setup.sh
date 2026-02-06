45490698a25ca0345be317c3ae44080c5c5363cc
ymkz <ymkz@users.noreply.github.com>
2026-02-07 01:50:47 +0900

    use oh-my-opencode

--- setup.sh
+++ setup.sh
@@ -52,7 +52,7 @@ if [[ -e "${HOME}/work/github.com/ymkz/dotfiles" ]]; then
   ln -nfs "${HOME}/work/github.com/ymkz/dotfiles/atuin/config.toml" "${HOME}/.config/atuin/config.toml"
   ln -nfs "${HOME}/work/github.com/ymkz/dotfiles/starship/starship.toml" "${HOME}/.config/starship.toml"
   ln -nfs "${HOME}/work/github.com/ymkz/dotfiles/opencode/opencode.jsonc" "${HOME}/.config/opencode/opencode.jsonc"
-  ln -nfs "${HOME}/work/github.com/ymkz/dotfiles/opencode/oh-my-opencode-slim.json" "${HOME}/.config/opencode/oh-my-opencode-slim.json"
+  ln -nfs "${HOME}/work/github.com/ymkz/dotfiles/opencode/oh-my-opencode.jsonc" "${HOME}/.config/opencode/oh-my-opencode.jsonc"
 fi
 
 # install rust (https://www.rust-lang.org/ja/tools/install)
