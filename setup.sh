d64b6374e3b33190b9dce9762c2a96fba7ddbe63
ymkz <ymkz@users.noreply.github.com>
2026-03-29 14:52:54 +0900

    update

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ XDG_CONFIG_HOME=${XDG_CONFIG_HOME:="${HOME}/.config"}
 XDG_CACHE_HOME=${XDG_CACHE_HOME:="${HOME}/.cache"}
 XDG_DATA_HOME=${XDG_DATA_HOME:="${HOME}/.local/share"}
 XDG_STATE_HOME=${XDG_STATE_HOME:="${HOME}/.local/state"}
-DOTFILES_DIR=${DOTFILES_DIR:="${HOME}/work/github.com/ymkz/dotfiles"}
+DOTFILES_DIR=${DOTFILES_DIR:="${HOME}/work/github.com/${USER}/dotfiles"}
 
 ### setup dir
 mkdir -p "${XDG_CONFIG_HOME}"
